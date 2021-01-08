dada759b67dc5c47486811981591e18b36f9a65e
chrissimusokwe <simusokwechristopher@gmail.com>
2021-01-08 20:17:02 +1100

    Invoicing (Listing and slider) 20210108 at 8.30pm

--- lib/models/InvoicingModel.dart
+++ lib/models/InvoicingModel.dart
@@ -66,4 +66,28 @@ class InvoicingModel {
     );
     addNewInvoices(invoicingModel.toMap());
   }
+
+  Future<void> uploadingData(String _productName, String _productPrice,
+      String _imageUrl, bool _isFavourite) async {
+    await FirebaseFirestore.instance.collection("products").add({
+      'productName': _productName,
+      'productPrice': _productPrice,
+      'imageUrl': _imageUrl,
+      'isFavourite': _isFavourite,
+    });
+  }
+
+  Future<void> editProduct(bool _isFavourite, String id) async {
+    await FirebaseFirestore.instance
+        .collection("products")
+        .doc(id)
+        .update({"isFavourite": !_isFavourite});
+  }
+
+  Future<void> deleteProduct(DocumentSnapshot doc) async {
+    await FirebaseFirestore.instance
+        .collection("products")
+        .doc(doc.id)
+        .delete();
+  }
 }
